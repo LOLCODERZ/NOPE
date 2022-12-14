3935fefa675bcd18b3a6f174a85d3f4054f8196d
Victoria Stephany Huisman Sigcha <hello.victoria@outlook.ie>
2022-12-14 10:05:47 +0100

    usage of stack_pop & stack_push

--- src/vm.cpp
+++ src/vm.cpp
@@ -1,64 +1,88 @@
 #include "vm.h"
 
+void VM::stack_push(uint8_t value) {
+    // Check if the stack is full
+    if (this->stack_pointer >= STACK_SIZE) {
+        this->overflow = true;
+        throw "ERROR: Stack overflow";
+    }
+    // Push the value onto the stack
+    this->stack[this->stack_pointer++] = value;
+}
+
+uint8_t VM::stack_pop() {
+    // Check if the stack is empty
+    if (this->stack_pointer == 0) {
+        this->underflow = true;
+        throw "ERROR: Stack underflow";
+    }
+    // Return the top element of the stack
+    return this->stack[--this->stack_pointer];
+}
+
 bool VM::execute_instruction(Instruction instruction, uint8_t arg0) {
     switch (instruction) {
         case Instruction::Push: {
             // Check if the stack is full
             if (this->stack_pointer >= STACK_SIZE) {
+                this->overflow = true;
                 throw "ERROR: Stack overflow";
-                return false;
             }
             // Push the argument onto the stack
-            this->stack[this->stack_pointer++] = arg0;
+            this->stack_push(arg0);
             } break;
 
         case Instruction::Add: {
             // Get the two operands from the stack: This goes for all following operation cases
-            uint8_t left_side = this->stack[--this->stack_pointer];
-            uint8_t right_side = this->stack[--this->stack_pointer];
+            uint8_t left_side = this->stack_pop();
+            uint8_t right_side = this->stack_pop();
             // Add them together and push the result onto the stack
-            this->stack[this->stack_pointer++] = left_side + right_side;
+            this->stack_push(left_side + right_side);
             
-            // Check under and overflow
-            if (this->stack[this->stack_pointer - 1] < left_side || this->stack[this->stack_pointer - 1] < right_side) {
-                return false;
+            // Check for overflow
+            if (left_side > 0xFF - right_side) {
+                this->overflow = true;
+                throw "ERROR: Stack overflow";
             }
         } break;
 
 
         case Instruction::Subtract: {
-            uint8_t left_side = this->stack[--this->stack_pointer];
-            uint8_t right_side = this->stack[--this->stack_pointer];
+            uint8_t left_side = this->stack_pop();
+            uint8_t right_side = this->stack_pop();
             // Subtract and push the result onto the stack
-            this->stack[this->stack_pointer++] = left_side - right_side;
+            this->stack_push(left_side - right_side);
 
-            // Check under and overflow
-            if (this->stack[this->stack_pointer - 1] > left_side || this->stack[this->stack_pointer - 1] > right_side) {
-                return false;
+            // Check overflow
+            if (left_side < right_side) {
+                this->overflow = true;
+                throw "ERROR: Stack overflow";
             }
         } break;
 
         case Instruction::Multiply: {
-            uint8_t left_side = this->stack[--this->stack_pointer];
-            uint8_t right_side = this->stack[--this->stack_pointer];
+            uint8_t left_side = this->stack_pop();
+            uint8_t right_side = this->stack_pop();
             // Multiply and push the result onto the stack
-            this->stack[this->stack_pointer++] = left_side * right_side;
+            this->stack_push(left_side * right_side);
 
-            // Check for under and overflow
-            if (this->stack[this->stack_pointer - 1] < left_side || this->stack[this->stack_pointer - 1] < right_side) {
-                return false;
+            // Check for overflow
+            if (left_side > 0xFF / right_side) {
+                this->overflow = true;
+                throw "ERROR: Stack overflow";
             }
         } break;
 
         case Instruction::Divide: {
-            uint8_t left_side = this->stack[--this->stack_pointer];
-            uint8_t right_side = this->stack[--this->stack_pointer];
+            uint8_t left_side = this->stack_pop();
+            uint8_t right_side = this->stack_pop();
             // Divide and push the result onto the stack
-            this->stack[this->stack_pointer++] = left_side / right_side;
+            this->stack_push(left_side / right_side);
 
-            // Check under and overflow
-            if (this->stack[this->stack_pointer - 1] > left_side || this->stack[this->stack_pointer - 1] < right_side) {
-                return false;
+            // Check for overflow
+            if (left_side % right_side != 0) {
+                this->overflow = true;
+                throw "ERROR: Stack overflow";
             }
         } break;
 
@@ -81,19 +105,19 @@ bool VM::execute_instruction(Instruction instruction, uint8_t arg0) {
         } break;
 
         case Instruction::JumpEqual: {
-            uint8_t lhs = this->stack[--this->stack_pointer];
-            uint8_t rhs = this->stack[this->stack_pointer];
+            uint8_t left_side = this->stack[--this->stack_pointer];
+            uint8_t right_side = this->stack[this->stack_pointer];
             // Check if the two operands are equal
-            if (lhs == rhs) {
+            if (left_side == right_side) {
                 this->program_counter = arg0;
             }
         } break;
 
         case Instruction::JumpNotEqual: {
-            uint8_t lhs = this->stack[--this->stack_pointer];
-            uint8_t rhs = this->stack[this->stack_pointer];
+            uint8_t left_side = this->stack[--this->stack_pointer];
+            uint8_t right_side = this->stack[this->stack_pointer];
             // Check if the two operands are not equal
-            if (lhs != rhs) {
+            if (left_side != right_side) {
                 this->program_counter = arg0;
             }
         } break;
@@ -101,8 +125,8 @@ bool VM::execute_instruction(Instruction instruction, uint8_t arg0) {
         case Instruction::Append: {
             // Check if there is enough space on the stack
             if (this->stack_pointer + 1 >= STACK_SIZE) {
+                this->overflow = true;
                 throw "ERROR: Stack overflow";
-                return false;
             }
             // Increment the size of the stack to make space for the new element
             this->stack_pointer++;
@@ -118,19 +142,11 @@ bool VM::execute_instruction(Instruction instruction, uint8_t arg0) {
     return true;
 }
 
-uint8_t VM::pop() {
-    // Check if the stack is empty
-    if (this->stack_pointer == 0) {
-        throw "ERROR: Stack underflow";
-        return 0;
-    }
-    // Return the top element of the stack
-    return this->stack[--this->stack_pointer];
-}
 
 void VM::debug() {
     std::cout << "----- Lolcode VM -----" << std::endl;
     std::cout << "Stack (" << this->stack_pointer << "): ";
+
     // Print stack contents
     for (uintptr_t i = 0; i < this->stack_pointer; i++) {
         if (i % 8 == 0) std::cout << std::endl << "      ";
